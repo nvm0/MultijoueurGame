e103cbaccf04e10d036a7b3d561ddfc53406adf2
Nvm_0 <nvm64527@gmail.com>
2020-11-07 19:35:49 +0100

    Hello world MultiplayerVersion

---
+++ Assets/Scripts/SimpleMove.cs
@@ -0,0 +1,21 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMove : NetworkBehaviour
+{
+    void HandleMovement()
+    {
+        if(isLocalPlayer){
+            float moveHor = Input.GetAxis("Horizontal");
+            float moveVert = Input.GetAxis("Vertical");
+            Vector3 movement = new Vector3(moveHor * 0.1f ,0, moveVert*0.1f);
+            transform.position = transform.position + movement;
+        }
+    }
+
+    void Update(){
+        HandleMovement();
+    }
+}
